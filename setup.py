0909326543d74dc62b0907a29a2c988bf7800303
Shuiskij Dmitry <darnesmeister@gmail.com>
2015-03-09 19:52:37 +0200

    correct dependencies

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ setup(
     name="bw",
     version='0.1',
     # uncomment the following lines if you fill them out in release.py
-    description='helper around modified ThinkGear protocol reader and PyBluez',
+    description='helper around modified ThinkGear protocol reader and PyBluez to read data from MindSet',
     author='Shuiskij Dmitry',
     author_email='darnesmeister@gmail.com',
     url='',
     #download_url=download_url,
     license='BSD',
     packages=packages,
-
+    requires=['pybluez', 'thinkgear'],
+    install_requires=['pybluez', 'thinkgear'],
+    dependency_links=['https://github.com/darnes/pythinkgear/releases/download/v0.3/thinkgear-0.3.tar.gz',],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
